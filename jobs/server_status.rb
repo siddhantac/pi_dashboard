85088ca2fd7ed769d05e6988533a114b9a0229e7
Siddhanta <sid.choco@gmail.com>
2016-02-10 22:50:39 +0800

    directory restructured. colors added. online/offline status color coded. tooltip added. title bar added

--- jobs/server_status.rb
+++ jobs/server_status.rb
@@ -2,7 +2,13 @@ check = 'running.'
 online_stat = "Online"
 offline_stat = "Offline"
 
-SCHEDULER.every '1m' do
+online_color = "teal darken-1"
+offline_color = "red darken-3"
+
+ssh_online_color = "indigo darken-3"
+ssh_offline_color = "red darken-4"
+
+SCHEDULER.every '5m' do
 
 nginx = `service nginx status | awk \'{printf $3}\'`
 sr = `service sickrage status | awk \'{printf $3}\'`
@@ -14,38 +20,48 @@ ssh = `service ssh status | awk \'{printf $3}\'`
 
 if nginx == check
 	nginx_stat = online_stat
+	nginx_color = online_color
 else
 	nginx_stat = offline_stat
+	nginx_color = offline_color
 end
 
 if sr == check
 	sr_stat = online_stat
+	sr_color = online_color
 else
 	sr_stat = offline_stat
+	sr_color = offline_color
 end
 
 if samba == check
 	samba_stat = online_stat
+	samba_color = online_color
 else
 	samba_stat = offline_stat
+	samba_color = offline_color
 end
 
 if txr == check
 	txr_stat = online_stat
+	txr_color = online_color
 else
 	txr_stat = offline_stat
+	txr_color = offline_color
 end
 
 if ssh == check
 	ssh_stat = online_stat
+	ssh_color = ssh_online_color
 else
 	ssh_stat = offline_stat
+	ssh_color = ssh_offline_color
 end
 
-send_event('nginxstatus', {text: nginx_stat} )
-send_event('srstatus', {text: sr_stat} )
-send_event('sambastatus', {text: samba_stat} )
-send_event('txrstatus', {text: txr_stat} )
-send_event('pistatus', {text: ssh_stat} )
+send_event('nginxstatus', {text: nginx_stat, status: nginx_color} )
+send_event('srstatus', {text: sr_stat, status: sr_color} )
+send_event('sambastatus', {text: samba_stat, status: samba_color} )
+send_event('txrstatus', {text: txr_stat, status: txr_color} )
+send_event('pistatus', {text: ssh_stat, status: ssh_color} )
 
 end
--- widgets/text/text.coffee
+++ widgets/text/text.coffee
@@ -1,1 +1,10 @@
 class Dashing.Text extends Dashing.Widget
+
+
+  onData: (data) ->
+    if data.status
+      # clear existing color classes
+      $(@get('node')).attr 'class', (i,c) ->
+        c.replace /\bgrey|teal darken-1|red darken-3|indigo darken-3|red darken-4/g, ''
+      # add new class
+      $(@get('node')).addClass "#{data.status}"
