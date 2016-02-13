3212415d5e501b7d23fdc13712263e26cdc11f84
Siddhanta <sid.choco@gmail.com>
2016-02-13 12:06:59 +0800

    Updated memory meter

--- jobs/meters.rb
+++ jobs/meters.rb
@@ -3,8 +3,10 @@ SCHEDULER.every '5s' do
 	
 	temp = `/home/pi/Desktop/dashing/proj2/jobs/temperature.sh`
 	mem_full = `free -om | grep "Mem:" | awk \'{print $2}'`
-	mem_used = `free -om | grep "Mem:" | awk \'{print $3}'`
-	mem_free = `free -om | grep "Mem:" | awk \'{print $4}'`
+	mem_used = `free -m | awk 'NR==3' | awk \'{print $3}'`
+
+	#Use free from cached
+	mem_free = `free -m | awk 'NR==3' | awk \'{print $4}'`
 
 	send_event('temperature', value: temp)
 	send_event('memory', value: mem_used) 
